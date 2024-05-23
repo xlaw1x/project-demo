6b9717f4f71f30a6b1c1c24c4119a5fa0bfd4036
xlaw1x <149936342+xlaw1x@users.noreply.github.com>
2024-05-23 19:03:01 +0800

    replace results page

--- pages/02_Results.py
+++ pages/02_Results.py
@@ -2,3 +2,7 @@ import streamlit as st
 
 st.title("Results")
 st.write("Here you can put each of your key results.")
+
+st.header("1. Tio customer spending behavior by category")
+st.image("../images/total_spending.png")
+st.write("Travel is the top spender's highest spending, followed by onsite grocery and onsite shopping")
